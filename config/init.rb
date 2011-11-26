d96d3fffeef834ce4e9d0e88dc1c646cb286c711
Harold Giménez <hgimenez@thoughtbot.com>
2011-11-26 12:37:59 -0500

    herokuize db connection

--- config/init.rb
+++ config/init.rb
@@ -1,6 +1,7 @@
 require 'sequel'
 require 'sinatra'
-DB = Sequel.connect("postgres:///rtfmroulette_#{settings.environment}")
+database_url = ENV['SHARED_DATABASE_URL'] || "postgres:///rtfmroulette_#{settings.environment}"
+DB = Sequel.connect(database_url)
 require 'sinatra/reloader' if development?
 Dir['lib/*'].each do |f|
   require File.expand_path(f, "#{File.dirname(__FILE__)}/..")
