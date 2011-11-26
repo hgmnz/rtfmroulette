01ec8197641d6d98f2b05ebd33d27ae171af1bcf
Harold Giménez <hgimenez@thoughtbot.com>
2011-11-25 21:39:42 -0500

    Articles?

--- rtfmroulette.rb
+++ rtfmroulette.rb
@@ -6,3 +6,8 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+get '/roulette' do
+  @article = { title: 'This is a test', body: 'This is the body of the manual' }
+  erb :article
+end
