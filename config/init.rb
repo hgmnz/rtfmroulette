cf2ed34c5d9163a4cd1387cec93c5cdc489b12cc
Harold Giménez <hgimenez@thoughtbot.com>
2011-11-27 00:21:28 -0500

    Sequel timestmaps plugin

--- config/init.rb
+++ config/init.rb
@@ -1,10 +1,15 @@
 require 'sequel'
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
 database_url = ENV['SHARED_DATABASE_URL'] || "postgres:///rtfmroulette_#{settings.environment}"
 DB = Sequel.connect(database_url)
-require 'sinatra/reloader' if development?
+Sequel::Model.plugin :timestamps
+
 Dir['lib/*'].each do |f|
   require File.expand_path(f, "#{File.dirname(__FILE__)}/..")
 end
 
 set :root, File.expand_path('..', File.dirname(__FILE__))
+
+
