3a24a1d39150be348bbb776d1ce6b4d45024f4f6
Harold Giménez <hgimenez@thoughtbot.com>
2011-11-26 23:04:35 -0500

    remove debugging

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -15,33 +15,26 @@ class Crawler
     @url   = url
     @root  = root
     @level = level
-    puts "Initialized a crawler with #{@url} at level #{@level}"
   end
 
   def crawl
     unless source_exists?
       store_content
-    else
-      puts "Not storing content, it already exists"
     end
     crawl_links
   end
 
   def content
-    puts "Grabbing content for "#{@url}"
     @content ||= open(@url).read rescue ''
   end
 
   def crawl_links
-    puts "Crawling links at level #{@level}"
     if @level < MAX_LEVEL
       links.each do |link|
         next unless link['href']
         next if %w(/ #).include? link['href'].strip
         absolute_url = (uri + link['href'].strip).to_s
-        puts "Processing link: #{absolute_url}"
         next unless absolute_url =~ /#{@root.crawl_scope}/
-        puts "Crawling #{absolute_url}"
         self.class.new(absolute_url, @root, @level + 1).crawl
       end
     end
