2d16b9565758f6414064914ce497c6c9d7af6ac2
Harold Giménez <hgimenez@thoughtbot.com>
2011-11-28 20:30:41 -0500

    Extract to methods on parser

--- lib/parser/default.rb
+++ lib/parser/default.rb
@@ -2,25 +2,32 @@ module Parser
   class Default
 
     def initialize(source)
-      @raw_content = source.content
-      @url         = source.url
+      @raw_content  = source.content
+      @url          = source.url
+      @readable_doc = extract_readable_doc
     end
 
     def readable_content
-      readable = Readability::Document.new(@raw_content, retry_length: 10).content
-      doc = Nokogiri::HTML(readable)
-      doc.css('a').each do |link|
+      convert_links_to_absolute
+      @readable_doc.to_html
+    end
+
+    protected
+    def convert_links_to_absolute
+      @readable_doc.css('a').each do |link|
         if link['href']
           absolute = (self.uri + link['href']).to_s
           link['href'] = absolute
         end
       end
-      doc.to_html
     end
 
     def uri
       URI.parse(@url)
     end
-    protected :uri
+
+    def extract_readable_doc
+      Nokogiri::HTML(Readability::Document.new(@raw_content, retry_length: 10).content)
+    end
   end
 end
